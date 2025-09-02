f879a451574daccb7634a36d889e4c7b2588cbd1
shlomo1839 <shlomo1839@gmail.com>
2025-09-02 11:47:10 +0300

    Delete encryption.py

--- encryption.py
+++
@@ -1,7 +0,0 @@
-class encryption:
-    def __init__(self, key):
-        self.key = key.encode('utf-8') if isinstance(key, str) else bytes([key])
-
-    def xor_crypt(self, text):
-        result = bytes([b ^ self.key[i % len(self.key)] for i, b in enumerate(text.encode('utf-8'))])
-        return result.decode('latin1')
